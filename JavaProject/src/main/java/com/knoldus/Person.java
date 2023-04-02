229f4a5416dad79e210387e820db4074acf0781b
SahilBabbar12 <sahil.babbar@knoldus.com>
2023-04-02 15:23:36 +0530

    person class having name and age as attributes

---
+++ JavaProject/src/main/java/com/knoldus/Person.java
@@ -0,0 +1,32 @@
+package com.knoldus;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getters and setters method to get and set the name and age of person.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
+
+
