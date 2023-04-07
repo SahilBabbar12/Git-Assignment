9834c19ec69dec6b6c1d82944fa7f555a674f443
Sahil Babbar <124916680+SahilBabbar12@users.noreply.github.com>
2023-04-07 20:39:57 +0530

    Update Main.java

--- JavaProject/src/main/java/com/knoldus/Main.java
+++ JavaProject/src/main/java/com/knoldus/Main.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter the name");
-        String name=input.next();
-        System.out.println("Enter the age");
-        Integer age= input.nextInt();
 
-        Person person =new Person(name,age);
+        Person person =new Person("sahil",23);
+        
         System.out.println("name: "+person.getName());
         System.out.println("age: "+person.getAge());
 
